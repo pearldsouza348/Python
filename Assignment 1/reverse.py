fffdff7757fdae42cf501f1fd83252019929516d
Pearl D'Souza <82046064+pearldsouza348@users.noreply.github.com>
2021-07-12 12:17:03 +0530

    Create reverse.py

---
+++ Assignment 1/reverse.py
@@ -0,0 +1,9 @@
+# Operating System List
+systems = ['Windows', 'MacOS', 'Linux']
+print('Original List:', systems)
+
+# List Reverse
+systems.reverse()
+
+# updated list
+print('Updated List:', systems)
