7a68ff54713d8e833879a551aed7ed029224302d
Pearl D'Souza <82046064+pearldsouza348@users.noreply.github.com>
2021-07-12 11:50:18 +0530

    Create list.py

---
+++ Assignment 1/list.py
@@ -0,0 +1,15 @@
+# List slicing in Python
+
+my_list = ['m','i','s','s','i','s','s','i','p','i']
+
+# elements 3rd to 5th
+print(my_list[2:5])
+
+# elements beginning to 4th
+print(my_list[:-5])
+
+# elements 6th to end
+print(my_list[5:])
+
+# elements beginning to end
+print(my_list[:])
