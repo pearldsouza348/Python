19e890ef2671a2266597d17209d1585f49110b5d
Pearl D'Souza <82046064+pearldsouza348@users.noreply.github.com>
2021-07-12 12:13:11 +0530

    Create index.py

---
+++ Assignment 1/index.py
@@ -0,0 +1,12 @@
+# vowels list
+vowels = ['a', 'e', 'i', 'o','u']
+
+# index of 'e' in vowels
+index = vowels.index('e')
+print('The index of e:', index)
+
+# element 'i' is searched
+# index of the first 'i' is returned
+index = vowels.index('i')
+
+print('The index of i:', index)
