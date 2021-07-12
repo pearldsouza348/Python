0495c63fcdd594d6bce9185a91ecb6d2e9306781
Pearl D'Souza <82046064+pearldsouza348@users.noreply.github.com>
2021-07-12 12:08:58 +0530

    Create pop.py

---
+++ Assignment 1/pop.py
@@ -0,0 +1,9 @@
+# programming languages list
+languages = ['Python', 'Java', 'SQL', 'French', 'HTML']
+
+# remove and return the 4th item
+return_value = languages.pop(3)
+print('Return Value:', return_value)
+
+# Updated List
+print('Updated List:', languages)
